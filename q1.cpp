c06d7c3451f825c18e33916d0b51ecb1c1cfbc82
sonwaneakash <42178604+sonwaneakash@users.noreply.github.com>
2018-11-18 23:12:33 +0530

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class Box {
+public:
+	// Defining length and breadth
+	double l, b, h;
+
+};
+
+
+
+
+int main(){
+
+Box Box1, Box2; // Define the objects Box1 & Box2 of type box
+double volume; // store volume of a box
+
+// Box1 specification
+Box1.l = 15;
+Box1.b = 12;
+Box1.h = 6;
+
+// Box2 specification
+Box2.l = 9;
+Box2.b = 13;
+Box2.h = 14;
+
+// volume of Box1
+volume = Box1.l*Box1.b*Box1.h;
+cout << "Volume of Box1 : " << volume << endl;
+
+
+// volume of Box2
+volume = Box2.l*Box2.b*Box2.h;
+cout << "Volume of Box2 : " << volume << endl;
+
+return 0;
+}
